995e5263f383e2ce38b89dfaa23bc1ba8ceacca4
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-08-16 13:14:43 -0400

    Collection management features

--- Tests/TuneOutModelTests/TuneOutModelTests.swift
+++ Tests/TuneOutModelTests/TuneOutModelTests.swift
@@ -10,7 +10,68 @@ let logger: Logger = Logger(subsystem: "TuneOutModel", category: "Tests")
 
 @available(macOS 13, *)
 final class TuneOutModelTests: XCTestCase {
-    #if false
+    @MainActor func testCollections() throws {
+        let station1 = APIStationInfo(stationuuid: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!, name: "Station 1", url: "https://radio-browser.info")
+        let station2 = APIStationInfo(stationuuid: UUID(uuidString: "00000000-0000-0000-0000-000000000002")!, name: "Station 1", url: "https://radio-browser.info")
+        let station3 = APIStationInfo(stationuuid: UUID(uuidString: "00000000-0000-0000-0000-000000000003")!, name: "Station 1", url: "https://radio-browser.info")
+        let station4 = APIStationInfo(stationuuid: UUID(uuidString: "00000000-0000-0000-0000-000000000004")!, name: "Station 1", url: "https://radio-browser.info")
+        let station5 = APIStationInfo(stationuuid: UUID(uuidString: "00000000-0000-0000-0000-000000000005")!, name: "Station 1", url: "https://radio-browser.info")
+
+        let db = try DatabaseManager(url: nil)
+        try db.initializeSchema()
+
+        XCTAssertEqual(try db.fetchCollections(standard: true).map(\.name), [StationCollection.favoritesCollectionName, StationCollection.recentsCollectionName])
+
+        let collection1 = try db.ctx.insert(StationCollection(name: "Collection 1", sortOrder: 1.0))
+        XCTAssertEqual(3, collection1.id) // starts at 3 because the first 2 are auto-initialized
+
+        let collection2 = try db.ctx.insert(StationCollection(name: "Collection 2", sortOrder: 2.0))
+        XCTAssertEqual(4, collection2.id)
+
+        let stored1 = try db.ctx.insert(StoredStationInfo(info: station1))
+        let stored2 = try db.ctx.insert(StoredStationInfo(info: station2))
+        let stored3 = try db.ctx.insert(StoredStationInfo(info: station3))
+        let stored4 = try db.ctx.insert(StoredStationInfo(info: station4))
+        let stored5 = try db.ctx.insert(StoredStationInfo(info: station5))
+
+        try db.addStation(stored1, toCollection: collection1)
+        try db.addStation(stored2, toCollection: collection1)
+        try db.addStation(stored3, toCollection: collection1)
+        try db.addStation(stored4, toCollection: collection1)
+        try db.addStation(stored5, toCollection: collection1)
+        try db.addStation(stored2, toCollection: collection1) // should move stored2 to top of queue
+
+        try db.addStation(stored4, toCollection: collection2)
+        try db.addStation(stored3, toCollection: collection2)
+
+        let stationCollections1 = try db.fetchStations(inCollection: collection1)
+        XCTAssertEqual(stationCollections1.map({ $0.0.id }), [stored2.id, stored5.id, stored4.id, stored3.id, /*stored2.id,*/ stored1.id])
+
+        let stationCollections2 = try db.fetchStations(inCollection: collection2)
+        XCTAssertEqual(stationCollections2.map({ $0.0.id }), [stored3.id, stored4.id])
+
+        let stored3Collections = try db.fetchStations(inCollection: collection2)
+        XCTAssertEqual(stored3Collections.map({ $0.0.id }), [stored3.id, stored4.id])
+
+        // check collection counts
+        let collectionCounts = try db.fetchCollectionCounts().filter {
+            !$0.0.isStandardCollection
+        }
+        XCTAssertEqual(collection2, collectionCounts.first?.0)
+        XCTAssertEqual(2, collectionCounts.first?.1)
+        XCTAssertEqual(collection1, collectionCounts.last?.0)
+        XCTAssertEqual(5, collectionCounts.last?.1)
+
+        try db.ctx.delete(instances: [stored1, stored2, stored3, stored4, stored5])
+        XCTAssertEqual(5, db.ctx.changes)
+
+        //try db.ctx.delete(instances: [storedCollection1, storedCollection2, storedCollection3, storedCollection4, storedCollection5])
+        XCTAssertEqual(0, try db.ctx.count(StationCollectionInfo.self), "relation rows should have been cascade deleted")
+    }
+
+    // MARK: API tests (disabled from macOS runs for performance)
+
+    #if os(iOS)
     func testCountryList() async throws {
         let countries = try await APIClient.shared.fetchCountries()
         XCTAssertTrue(countries.map(\.name).contains("US"))
@@ -94,54 +155,4 @@ final class TuneOutModelTests: XCTestCase {
         XCTAssertEqual("retrieved station url", response.message)
     }
     #endif
-
-    @MainActor func testCollections() throws {
-        let station1 = APIStationInfo(stationuuid: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!, name: "Station 1", url: "https://radio-browser.info")
-        let station2 = APIStationInfo(stationuuid: UUID(uuidString: "00000000-0000-0000-0000-000000000002")!, name: "Station 1", url: "https://radio-browser.info")
-        let station3 = APIStationInfo(stationuuid: UUID(uuidString: "00000000-0000-0000-0000-000000000003")!, name: "Station 1", url: "https://radio-browser.info")
-        let station4 = APIStationInfo(stationuuid: UUID(uuidString: "00000000-0000-0000-0000-000000000004")!, name: "Station 1", url: "https://radio-browser.info")
-        let station5 = APIStationInfo(stationuuid: UUID(uuidString: "00000000-0000-0000-0000-000000000005")!, name: "Station 1", url: "https://radio-browser.info")
-
-        let db = try DatabaseManager(url: nil)
-        try db.initializeSchema()
-
-        XCTAssertEqual(try db.fetchCollections(standard: true).map(\.name), [StationCollection.favoritesCollectionName, StationCollection.recentsCollectionName])
-
-        let collection1 = try db.ctx.insert(StationCollection(name: "Collection 1", sortOrder: 1.0))
-        XCTAssertEqual(3, collection1.id) // starts at 3 because the first 2 are auto-initialized
-
-        let collection2 = try db.ctx.insert(StationCollection(name: "Collection 2", sortOrder: 2.0))
-        XCTAssertEqual(4, collection2.id)
-
-        let stored1 = try db.ctx.insert(StoredStationInfo(info: station1))
-        let stored2 = try db.ctx.insert(StoredStationInfo(info: station2))
-        let stored3 = try db.ctx.insert(StoredStationInfo(info: station3))
-        let stored4 = try db.ctx.insert(StoredStationInfo(info: station4))
-        let stored5 = try db.ctx.insert(StoredStationInfo(info: station5))
-
-        try db.addStation(stored1, toCollection: collection1)
-        try db.addStation(stored2, toCollection: collection1)
-        try db.addStation(stored3, toCollection: collection1)
-        try db.addStation(stored4, toCollection: collection1)
-        try db.addStation(stored5, toCollection: collection1)
-        try db.addStation(stored2, toCollection: collection1) // should move stored2 to top of queue
-
-        try db.addStation(stored4, toCollection: collection2)
-        try db.addStation(stored3, toCollection: collection2)
-
-        let stationCollections1 = try db.fetchStations(inCollection: collection1)
-        XCTAssertEqual(stationCollections1.map({ $0.0.id }), [stored2.id, stored5.id, stored4.id, stored3.id, /*stored2.id,*/ stored1.id])
-
-        let stationCollections2 = try db.fetchStations(inCollection: collection2)
-        XCTAssertEqual(stationCollections2.map({ $0.0.id }), [stored3.id, stored4.id])
-
-        let stored3Collections = try db.fetchStations(inCollection: collection2)
-        XCTAssertEqual(stored3Collections.map({ $0.0.id }), [stored3.id, stored4.id])
-
-        try db.ctx.delete(instances: [stored1, stored2, stored3, stored4, stored5])
-        XCTAssertEqual(5, db.ctx.changes)
-
-        //try db.ctx.delete(instances: [storedCollection1, storedCollection2, storedCollection3, storedCollection4, storedCollection5])
-        XCTAssertEqual(0, try db.ctx.count(StationCollectionInfo.self), "relation rows should have been cascae deleted")
-    }
 }
